1a2c24a661bc11f120ae24d3aefb1b09ae71186c
Vinney Cavallo <vcavallo@gmail.com>
2013-01-15 22:13:14 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   #get "static_pages/home"
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root to: 'static_pages#home'
 
-  match '/help', to: 'static_pages#help'
-
-  match '/about', to: 'static_pages#about'
-
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
